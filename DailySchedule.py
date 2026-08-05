d79b3a576f33d5165e40f31f12883219797c7bd7
Daveblaze913 <davidekpenyong478@gmail.com>
2026-08-05 17:50:51 +0100

    First program

---
+++ DailySchedule.py
@@ -0,0 +1,24 @@
+#printing a statement
+print("7:00am-8:00am:I wake up and do my daily activities ")
+#printing a statement
+print("8:00am-9:00am:After that  read my bible for 1 hour")
+#printing a statement
+print("9:00am-10am:Study time for 1 hour")
+#printing a statement
+print("10:00am-11:00am:Play ROBLOX and talk with friends")
+#printing a statement
+print("11:00am-12:00pm: read books in my libary")
+#printing a statement
+print("12:00pm-2:00pm: Play games again for 2 hrs")
+#printing a statement
+print("2:00pm- 4:00pm: Do outdoor activities" )
+#printing a statement
+print("4:00pm-5:00pm:Read my bible while preparing coding classes")
+#printing a statement
+print("5:00pm-6:30pm: Have my codingal classes")
+#printing a statement
+print("6:30pm-8:00pm: Have my bath and finish eating")
+#printing a statement
+print("8:00pm-10:45pm: play games a liltle bit longer before i go to bed")
+#printing addition
+print(1+ 1+ 1 )
---
+++ FirstProgram.py
@@ -0,0 +1,6 @@
+#printing a statement
+print("Welcome to codingal")
+#printing a number
+print(56)
+#printing addition
+print(10 + 5)
