d605a7211e5fbad0f0ac40f877b1da363f9ceecf
Daveblaze913 <davidekpenyong478@gmail.com>
2026-08-14 19:36:54 +0100

    use of and and not

---
+++ Bitwise operators.py
@@ -0,0 +1,13 @@
+a = 20
+b = 47
+
+if a>20 & a<=50 :
+    print("Both are True")
+else:
+    print("Atleast one is False")
+
+if a>30 | b>50:
+    print("atleast one is true")
+
+if ~a <15:
+    print("True")
