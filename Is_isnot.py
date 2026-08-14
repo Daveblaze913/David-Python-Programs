848cf705790ebeb34dcdabad52b26498abc72408
Daveblaze913 <davidekpenyong478@gmail.com>
2026-08-14 19:29:13 +0100

    New and updated is not

---
+++ Is_isnot.py
@@ -0,0 +1,26 @@
+num = 54
+
+
+if type(num) is int:
+    print("True")
+else :
+    print("False")
+
+
+num = 54.2
+
+
+if type(num) is float:
+    print("True")
+else :
+    print("False")
+
+
+a = 20
+b = 20
+
+if a is b :
+    print("a and b are identical")
+
+if a is not num :
+    print("a and num are different")
