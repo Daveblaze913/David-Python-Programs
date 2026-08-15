f92703151796aabe681a162dfe18ec9132666ba5
Daveblaze913 <davidekpenyong478@gmail.com>
2026-08-15 10:35:55 +0100

    Libary visit planner

--- Libary.py
+++ Libary.py
@@ -6,4 +6,17 @@ weather = input  ("Enter today's weather condition (Sunny/Rainy/Cloudy)"). strip
 book = input ("Which genre of  books will you like to read (Action/Informative/Romantic)"). strip() .lower()
 
 print()
-print ("Your b")
+print ("Your book genre for the day is:", book)
+
+print("-"*50)
+
+if day == ("Monday"):
+    print("What book are u reading from the book you borrowed on sunday")
+elif day in ("Tuesday, Wednesday, Thursday, Friday"):
+    print("How is the progress of your book doing if you want to read another oen the libary is always open on weekdays")
+elif day == ("Saturday"):
+    print("It's one more day till the time to have your book is over I suggest you finish reading what you have now")
+elif day == ("Sunday"):
+    print("Its sunday it is time to submit the book you're reading feel free to borrow another one")
+else:
+    print("Error i can not find the day you are talking about")
