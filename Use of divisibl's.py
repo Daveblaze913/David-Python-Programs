9c30e0b459ffda7d33213888e866a9f2e0f39dfc
Daveblaze913 <davidekpenyong478@gmail.com>
2026-08-18 17:33:53 +0100

    use of divisble numbers

---
+++ Use of divisibl's.py
@@ -0,0 +1,7 @@
+numerator = int (input("Enter your numerator  "))
+denominator = int (input("Enter your denominator  "))
+
+if numerator % denominator ==0:
+    print("Numerator is Divisible by denominator")
+else:
+    print("Numerator is  not Divisible by denominator")
