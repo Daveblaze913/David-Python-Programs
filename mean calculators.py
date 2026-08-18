58f00a6332ef6ab6571dfe3fcf0b95c6b65d4b73
Daveblaze913 <davidekpenyong478@gmail.com>
2026-08-18 17:47:36 +0100

    Use of means

---
+++ mean calculators.py
@@ -0,0 +1,13 @@
+total_number = 40
+mean = 38
+wrong_number = 36
+right_number = 56
+
+sum = total_number*mean
+print("Sum of all 40 numbers ;",sum)
+
+num2 = sum - ((wrong_number) - (right_number))
+print("New num2 is wrong_number - right number")
+
+mean2 = num2 /total_number
+print("Updated mean : ",mean2)
