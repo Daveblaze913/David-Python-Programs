9347906bbce65b53bfee4b703953995c41432b89
Daveblaze913 <davidekpenyong478@gmail.com>
2026-08-18 17:20:48 +0100

    use of equations in phyton

---
+++ Simple equations.py
@@ -0,0 +1,16 @@
+w = 4
+v = 5
+x = 8
+y = 7
+
+output = (v+w)*x/y
+
+print (" Value of (v+w)*x/y - ",output)
+
+name = "David"
+age = 11
+
+if name == "David" or name == "Shipa" and age <= 10:
+    print("Hello world")
+else: 
+    print("Good bye")
