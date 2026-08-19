c3114739571828bba44569f8f587f279b6e02474
Daveblaze913 <davidekpenyong478@gmail.com>
2026-08-19 22:14:07 +0100

    daily activity planner

---
+++ .Personal goal collector.py
@@ -0,0 +1,13 @@
+name = input("Enter your name ")
+personal_goal = input("Enter your personal goal")
+
+age = 10
+target_month = 6
+
+print("Name:", name ,"->Type",type (name))
+print("Personal goal:", personal_goal ,"->Type",type (personal_goal))
+print("Age:", age ,"->Type",type (age))
+print("Target Month:", target_month ,"->Type",type (target_month))
+
+import keyword
+print(keyword.kwlist)
--- Daily _activity planner.py
+++ Daily _activity planner.py
@@ -1,1 +1,30 @@
 print("=============Daily activity planner===============")
+
+day = input("Enter your day Monday - Sunday: ").strip().capitalize()
+weather = input("What is the weather for today? (Sunny/Rainy/Cloudy) : ").strip().lower()
+chores = input("Have you  done your chores? (Yes/No): ").strip().lower()
+
+print()
+print("===================== Your plan for ", day , "=========================")
+
+print("-"*50)
+
+if day in ("Saturday","Sunday"):
+    print("Day type : weekend time i spend most of the day playing and reading after praying")
+elif day == "Monday":
+    print ("Day type : First day of  week after praying. I go outside and play for most of the day")
+elif day == "Friday":
+    print("Day type : Last day of your week/school")
+elif day in ("Tuesday","Wednesday","Thursday"):
+    print("Day type : Theses are  days i visit my grandma neighbours and cousins after my work is done. or read the bible")
+else:
+    print("ERROR day is not spelled correctly ! ⚠️ Kindly check spelling")
+
+if weather == "sunny" and chores  == "yes" :
+   print (" then go to the park as chores is complete")
+
+if weather == "cloudy" or weather == "rainy":
+    print("Weather tip : 🌧️ Take an umbrella with you.") 
+
+if weather == "rainy" and not chores == "yes":
+    print("Stay home and do chores ")
