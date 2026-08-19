973ea94132d807cd8ff51f69831ed5e24cbf0bbb
Daveblaze913 <davidekpenyong478@gmail.com>
2026-08-19 18:05:27 +0100

    Cyclist speed

---
+++ Cyclist speed mean.py
@@ -0,0 +1,15 @@
+cyclist1 = 10
+cyclist2 = 20
+cyclist3  = 30
+
+totalspeed = cyclist1+cyclist2+cyclist3
+avg_speed = totalspeed/3
+
+print("The average speed for all 3 cyclist is:", avg_speed)
+
+if cyclist1 < avg_speed:
+    print(" Cyclist1 is slower than avg speed")
+elif cyclist2 < avg_speed:
+    print("Cyclist 2 is slower than avg speed")
+elif cyclist3 < avg_speed:
+    print("Cyclist3 is  slower than the average")
