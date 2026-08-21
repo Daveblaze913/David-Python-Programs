f8792643e5c2d2220050870ffafd64089f3ad06f
Daveblaze913 <davidekpenyong478@gmail.com>
2026-08-21 19:20:24 +0100

    use of for loops in sum of whole numbers

---
+++ School memebrship.py
@@ -0,0 +1,6 @@
+Name = str(input("Enter your name"))
+School = str(input("Enter marks for English: "))
+Age = int(input("Enter marks for Science: "))
+Gender = str(input("Enter marks for Geography: "))
+Grade = int(input("Enter grade to enmter school: "))
+
