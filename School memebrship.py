e4b67c449dc36e6c24d668b641d62c3ed9669ac7
Daveblaze913 <davidekpenyong478@gmail.com>
2026-08-21 21:47:12 +0100

    School acces

--- School memebrship.py
+++ School memebrship.py
@@ -1,6 +1,20 @@
-Name = str(input("Enter your name"))
-School = str(input("Enter marks for English: "))
-Age = int(input("Enter marks for Science: "))
-Gender = str(input("Enter marks for Geography: "))
-Grade = int(input("Enter grade to enmter school: "))
+Name = str(input("Enter your name : "))
+School = str(input("Enter The school name: "))
+Age = str(input("Enter your age : "))
+Gender = str(input("State your gender : "))
+Grade = int(input("Enter grade to enter school : "))
 
+validRange = range (50,101)
+
+if Grade not in validRange:
+    print("Invalid input")
+elif Grade in range (91, 101):
+    print("Outstanding")
+elif Grade in range (75,101):
+    print("Distinction")
+elif Grade in range (60, 75):
+    print("1st class")
+elif Grade in range (50,60):
+    print("2nd class")
+else:
+    print("Failed ! try harder next time you can do it")
